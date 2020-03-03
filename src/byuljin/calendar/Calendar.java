557e44c4b964de479eec497e4fa8c39579d94278
byuljin1 <bjkimg@gmail.com>
2020-03-03 22:56:31 +0900

    update

--- src/byuljin/calendar/Calendar.java
+++ src/byuljin/calendar/Calendar.java
@@ -3,13 +3,13 @@ package byuljin.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
-	private final int[] MAX_DAYS ={ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; 
-	
+
+	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month -1];
+		return MAX_DAYS[month - 1];
 	}
-	
+
 	public void printSampleCal() {
 		System.out.println(" 일   월   화   수   목   금   토   ");
 		System.out.println("-------------------- ");
@@ -24,14 +24,26 @@ public class Calendar {
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		
-		System.out.println("달을 입력하세요");
-		int month = scanner.nextInt();
-		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
-		
-		cal.printSampleCal();
-		
+		String prompt = "cal> ";
+
+
+		int month = 1;
+		while (true) {
+			System.out.println("달을 입력하세요");
+			System.out.print(prompt);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+
+			if (month > 12) {
+				continue;
+			}
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		}
+
+		System.out.println("Bye~");
+
 		scanner.close();
 	}
 
